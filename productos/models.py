20a1f7c7fe95e51956b98dc2b5b6e5e27f9b3b81
Fernanda0517 <fernandarodriguez0517@gmail.com>
2023-09-18 10:05:43 -0500

    first commit

--- productos/models.py
+++ productos/models.py
@@ -1,17 +1,20 @@
 from django.db import models
-from django.core.validators import MinValueValidator
 from tipos_productos.models import TipoProducto
 from django.utils import timezone
 
 class Producto(models.Model):
     id = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=50)
-    codigo_barras = models.PositiveIntegerField(validators=[MinValueValidator(13)])
-    precio = models.DecimalField(max_digits=10, decimal_places=2, default=0, validators=[MinValueValidator(0)])
-    fecha_caducidad = models.DateField(validators=[MinValueValidator(timezone.now().date())])
-    cantidad_inventario = models.PositiveIntegerField(validators=[MinValueValidator(0)])
+    nombre = models.CharField(max_length=100)
+    codigo_barras = models.CharField(max_length=13, unique=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha_caducidad = models.DateField()
+    #precio_costo = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    #precio_venta = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    #precio_mayoreo = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    #fecha_registro = models.DateTimeField(default=timezone.now)
+    cantidad_inventario = models.PositiveIntegerField()
+    #inventario_minimo = models.PositiveIntegerField(default=0)
     tipo = models.ForeignKey(TipoProducto, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nombre
-
